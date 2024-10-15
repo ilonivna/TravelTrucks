49dd20bf91c26f0f2241b7e730a3e9b027955673
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-10-15 23:46:53 +0300

    homepage completed

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import css from "./Navigation.module.css";
+import logo from "../../assets/TravelTrucks.png";
+import clsx from "clsx";
 
+const getLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.active);
+  };
 
 export default function Navigation() {
-
-    
-    return (
-        <nav>
-            <NavLink to="/">
-                Home
-            </NavLink>
-            <NavLink to="/catalog">
-                Catalog
-            </NavLink>
-        </nav>
-    )
+  return (
+    <div className={css.container}>
+      <Link to="/">
+        <img src={logo} alt="logo" width="135" height="15" className={css.logo}/>
+      </Link>
+      <nav className={css.linkList}>
+        <NavLink to="/" className={getLinkClass}>Home</NavLink>
+        <NavLink to="/catalog" className={getLinkClass}>Catalog</NavLink>
+      </nav>
+    </div>
+  );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,20 @@
 import Button from "../../components/Button/Button.jsx";
-import Navigation from "../../components/Navigation/Navigation.jsx";
 import css from "./HomePage.module.css";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
+  
         <div className={css.container}>
-            <Navigation/>
+            <div>
             <h1>
             Campers of your dreams
             </h1>
-            <p>You can find everything you want in our catalog</p>
-            <Link to="/campers">
-            <Button>View Now</Button></Link>
+            <p>You can find everything you want in our catalog</p></div>
+            <div><Link to="/campers">
+            <Button>View Now</Button></Link></div>
+            
         </div>
+    
     )
 }
